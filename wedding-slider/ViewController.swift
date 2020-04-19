6955e3fd0a089564449b87960096fbac41207526
Lydia-Lu-code <guipopo5@gmail.com>
2020-04-19 20:20:02 +0800

    Initial Commit

---
+++ wedding-slider/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  wedding-slider
+//
+//  Created by 維衣 on 2020/4/19.
+//  Copyright © 2020 Lydia-L. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
